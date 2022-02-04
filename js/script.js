4cb406b82cfe8d347d4e3cf48559e5c6b92210dc
snacksident <leak187@gmail.com>
2022-02-04 14:59:41 -0800

    add soft reset (pause?) functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 /*DOM SELECTORS*/
 const canvas = document.querySelector("#canvas")
 const scoreBoard = document.querySelector(".score-board")
+const resetButton = document.querySelector(".reset-button")
+const playButton = document.querySelector(".play-button")
+//clears gameloop interval, rendering game over.  also clears canvas to blank state.
+resetButton.addEventListener('click',()=>{
+    clearInterval(gameLoopInterval)
+})
+playButton.addEventListener('click',()=>{
+    // let gameLoopInterval = setInterval(gameLoop, 500) //game speed set by interval
+})
+
 document.addEventListener('keydown',(e)=>{
     if(e.key === "a"){
-        
-        //player.throwPaper()
+        console.log(`user pressed ${e.key}`)
+        //when button is pressed
+        //construct a new Newspaper object at deliverypersons x,y
+        //render the object
+        //get object to move correctly
     }else if(e.key === "d"){
-       // player.throwPaper()
+       console.log(`user pressed ${e.key}`)
     }
 })
 
@@ -17,12 +30,10 @@ const ctx = canvas.getContext('2d')
 canvas.setAttribute("height", getComputedStyle(canvas)["height"])
 canvas.setAttribute("width", getComputedStyle(canvas)["width"])
 let userScore = 0
-let gameLoopInterval = setInterval(gameLoop, 1000) //game speed set by interval
+
+let gameLoopInterval = setInterval(gameLoop, 500) //game speed set by interval
 
 /* GAME FUNCTIONS */
-
-
-
 
 //houses all have a standard size.  we only pass in location of the house.
 class House {
@@ -37,12 +48,12 @@ class House {
         ctx.lineWidth = 2
         ctx.strokeRect(this.x,this.y,this.width,this.height)
     }
-    //change the y axis of the house, making it feel like you are moving.
+    //change the y axis of the house, simulating movement.
     slide(){
         this.y++
     }
 }
-
+//for the paperperson
 class Deliverer {
     constructor(xLoc, yLoc){
         this.x = xLoc,
@@ -59,9 +70,8 @@ class Deliverer {
         
     }
 }
-
+//for the thrown newspapers
 class Newspaper {
-    static paperCount = 0
     constructor(newX,newY){
         this.x = newX
         this.y = newY
@@ -72,7 +82,9 @@ class Newspaper {
         ctx.strokeStyle = "white"
         ctx.lineWidth = 1
         ctx.strokeRect(this.x,this.y,this.width,this.height)
+        // ctx.strokeRect(newPlayer.x,newPlayer.y,this.width,this.height)
     }
+    //the paper flies 2px right and 1px down per cycle - staying in line with the houses but heading towards them - left and right
     flyRight(){
         this.x += 2
         this.y++
@@ -82,8 +94,6 @@ class Newspaper {
         this.y++
     }
 }
-
-
 
 let newHouse = new House(5,5)
 let newPlayer = new Deliverer(300,300)
@@ -96,17 +106,16 @@ let newPaper = new Newspaper(300,300)
 //every tick, move all houses down the y axis.
 function gameLoop() {
     //generate houses(neighborhood)
-    //'slide' through neighborhood until timer runs out or finish line is it
     ctx.clearRect(0,0, canvas.width, canvas.height)
     scoreBoard.innerText = userScore
-    // newHouse.render()
+
+    //testing functionality
+    newHouse.render()
     newPlayer.render()
-    // newPaper.render()
+    newPaper.render()
+    newPaper.flyRight()
     newHouse.slide()
+    
 }
 
 
-//when button is pressed
-//construct a new Newspaper object
-//render the object on the delivery person
-//get object to move correctly
