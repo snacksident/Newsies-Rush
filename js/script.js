fb47f586ce699da43bf209229859ad579bd91a06
snacksident <leak187@gmail.com>
2022-02-04 17:29:12 -0800

    attempt solving event listener issue

--- js/script.js
+++ js/script.js
@@ -6,20 +6,25 @@ const playButton = document.querySelector(".play-button")
 //clears gameloop interval, rendering game over.  also clears canvas to blank state.
 resetButton.addEventListener('click',()=>{
     clearInterval(gameLoopInterval)
+    ctx.clearRect(0,0, canvas.width, canvas.height)
 })
 playButton.addEventListener('click',()=>{
-    // let gameLoopInterval = setInterval(gameLoop, 500) //game speed set by interval
+    gameLoopInterval = setInterval(gameLoop, 500) //game speed set by interval
+
 })
 
 document.addEventListener('keydown',(e)=>{
-    if(e.key === "a"){
-        console.log(`user pressed ${e.key}`)
-        //when button is pressed
-        //construct a new Newspaper object at deliverypersons x,y
-        //render the object
-        //get object to move correctly
-    }else if(e.key === "d"){
-       console.log(`user pressed ${e.key}`)
+    for(let i = 0; i < paperArray.length; i++){
+        if(e.key === "a"){
+            //displays flying paper when key is held.  need to somehow call these functions in the gameloop when event/keypress happens.
+            paperArray[i].render()
+            paperArray[i].flyLeft()
+            console.log(`user pressed ${e.key}`)
+        }else if(e.key === "d"){
+            paperArray[i].render()
+            paperArray[i].flyRight()
+            console.log(`user pressed ${e.key}`)
+        }
     }
 })
 
@@ -31,8 +36,8 @@ canvas.setAttribute("height", getComputedStyle(canvas)["height"])
 canvas.setAttribute("width", getComputedStyle(canvas)["width"])
 let userScore = 0
 
-let gameLoopInterval = setInterval(gameLoop, 500) //game speed set by interval
-
+// let gameLoopInterval = setInterval(gameLoop, 500) //game speed set by interval
+let gameLoopInterval;
 /* GAME FUNCTIONS */
 
 //houses all have a standard size.  we only pass in location of the house.
@@ -82,7 +87,6 @@ class Newspaper {
         ctx.strokeStyle = "white"
         ctx.lineWidth = 1
         ctx.strokeRect(this.x,this.y,this.width,this.height)
-        // ctx.strokeRect(newPlayer.x,newPlayer.y,this.width,this.height)
     }
     //the paper flies 2px right and 1px down per cycle - staying in line with the houses but heading towards them - left and right
     flyRight(){
@@ -98,10 +102,9 @@ class Newspaper {
 let newHouse = new House(5,5)
 let newPlayer = new Deliverer(300,300)
 let newPaper = new Newspaper(300,300)
-
-//create paperboy
-
-    //make paperboy throw newspaper
+let newPaper2 = new Newspaper(300,300)
+let newPaper3 = new Newspaper(300,300)
+let paperArray = [newPaper, newPaper2, newPaper3]
 
 //every tick, move all houses down the y axis.
 function gameLoop() {
@@ -109,13 +112,31 @@ function gameLoop() {
     ctx.clearRect(0,0, canvas.width, canvas.height)
     scoreBoard.innerText = userScore
 
-    //testing functionality
-    newHouse.render()
-    newPlayer.render()
-    newPaper.render()
-    newPaper.flyRight()
-    newHouse.slide()
+    //when key is pressed - get next newspaper out of newspaper array
+    //render newspaper, set newspaper in motion
+
     
+
+
+    //        weston recommendation for letting papers fly
+    // for(let i = 0; i < paperArray.length; i++){
+    //     paperArray[i].render()
+    //     if(e.key === "a"){
+    //         paperArray[i].flyLeft
+    //     }
+    //     if(e.key === "d"){
+    //         paperArray[i].flyRight
+    //     }
+    // }
+
+
+
+    //testing functionality
+    // newHouse.render()
+    // newPlayer.render()
+    // newPaper.render()
+    // newPaper.flyRight()
+    // newHouse.slide()
 }
 
 
