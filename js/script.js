e73df3929f17b78656bdf65262c182c63725578e
snacksident <leak187@gmail.com>
2022-02-09 11:20:16 -0800

    refactor object declartions - objects now generated inside functions that are invoked on start button press

--- js/script.js
+++ js/script.js
@@ -11,10 +11,16 @@ resetButton.addEventListener("click", () => {
     clearInterval(gameLoopInterval)
     resetGameState()
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    gameRunning = false
     //reset all game-state values in here
 })
 playButton.addEventListener("click", () => {
-    gameLoopInterval = setInterval(gameLoop, 60) //game speed set by interval
+    if ((gameRunning === false)) {
+        makeHouses(5)
+        makeNewspapers(5)
+        gameLoopInterval = setInterval(gameLoop, 60) //game speed set by interval
+        gameRunning = true
+    }
 })
 
 //tracking keypresses inside object with boolean values.
@@ -27,16 +33,18 @@ const ctx = canvas.getContext("2d")
 canvas.setAttribute("height", getComputedStyle(canvas)["height"])
 canvas.setAttribute("width", getComputedStyle(canvas)["width"])
 let userScore = 0
+let gameRunning = false
 
 // currently turning gameloopinterval on and off with the "play game" and "end game" buttons. end game clears screen but pauses object statuses.
 let gameLoopInterval
+let paperPlaceInterval
 
 /* GAME FUNCTIONS */
 
 //houses all have a standard size.  we only pass in location of the house.
 class House {
     constructor(xLoc, yLoc) {
-        (this.x = xLoc), (this.y = yLoc), (this.width = 50), (this.height = 80)
+        ;(this.x = xLoc), (this.y = yLoc), (this.width = 50), (this.height = 80)
         this.isSubscriber = subscriberRandomizer()
         this.isDelivered = false
     }
@@ -52,7 +60,7 @@ class House {
         ctx.fillStyle = "salmon"
         ctx.lineWidth = 3
         ctx.strokeRect(this.x, this.y, this.width, this.height)
-        ctx.fillRect(this.x,this.y,this.width,this.height)
+        ctx.fillRect(this.x, this.y, this.width, this.height)
     }
     //change the y axis of the house, simulating movement.
     slide() {
@@ -62,11 +70,14 @@ class House {
 //for the paperperson
 class Deliverer {
     constructor(xLoc, yLoc) {
-        (this.x = xLoc), (this.y = yLoc), (this.width = 4), (this.height = 15), (this.hasPowerup = false)
+        ;(this.x = xLoc),
+            (this.y = yLoc),
+            (this.width = 4),
+            (this.height = 15),
+            (this.hasPowerup = false)
     }
     render() {
-        if(this.hasPowerup){
-
+        if (this.hasPowerup) {
         }
         ctx.fillStyle = "green"
         ctx.lineWidth = 2
@@ -111,60 +122,21 @@ class Powerup {
     }
 }
 
-let powerup1 = new Powerup(getRandomInBoundsXValue(),getRandomInBoundsYValue())
-let powerup2 = new Powerup(200,200)
-let powerup3 = new Powerup(250,200)
-let powerupArray = [powerup1,powerup2,powerup3]
+let powerup1 = new Powerup(getRandomInBoundsXValue(), getRandomInBoundsYValue())
+let powerup2 = new Powerup(200, 200)
+let powerup3 = new Powerup(250, 200)
+let powerupArray = [powerup1, powerup2, powerup3]
 let placedPowerups = []
-
-let leftHouse = new House(5, 5)
-let leftHouse2 = new House(5, 115)
-let leftHouse3 = new House(5, 225)
-let leftHouse4 = new House(5, 335)
-let leftHouse5 = new House(5, 445)
-
-let rightHouse = new House(600, 5)
-let rightHouse2 = new House(600, 100)
-let rightHouse3 = new House(600, 230)
-let rightHouse4 = new House(600, 350)
-let rightHouse5 = new House(600, 460)
 
 let newPlayer = new Deliverer(300, 300)
 
-let newPaper = new Newspaper(newPlayer.x, newPlayer.y)
-let newPaper2 = new Newspaper(newPlayer.x, newPlayer.y)
-let newPaper3 = new Newspaper(newPlayer.x, newPlayer.y)
-let newPaper4 = new Newspaper(newPlayer.x, newPlayer.y)
-let newPaper5 = new Newspaper(newPlayer.x, newPlayer.y)
-
 let bonusPaper = new Newspaper(newPlayer.x, newPlayer.y)
 
-let paperArray = [newPaper, newPaper2, newPaper3, newPaper4, newPaper5]
-let neighborhood = [
-    leftHouse,
-    rightHouse,
-    leftHouse2,
-    rightHouse2,
-    leftHouse3,
-    rightHouse3,
-    leftHouse4,
-    rightHouse4,
-    leftHouse5,
-    rightHouse5,
-]
-let neighborhoodLeft = [
-    leftHouse,
-    leftHouse2,
-    leftHouse3,
-    leftHouse4,
-    leftHouse5,
-]
+let paperArray = []
+let neighborhood = []
+let neighborhoodLeft = []
 let neighborhoodRight = [
-    rightHouse,
-    rightHouse2,
-    rightHouse3,
-    rightHouse4,
-    rightHouse5,
+    
 ]
 let thrownPapersRight = []
 let thrownPapersLeft = []
@@ -181,10 +153,34 @@ function houseMover() {
     })
 }
 
+//maybe create set amount of houses in a function. push them into the correct arrays
+function makeHouses(n) {
+    let houses = new Array(n)
+    let yLoc = 5
+    //generates houses on left side of level
+    for (let i = 0; i < n; i++) {
+        houses[i] = new House(5,yLoc)
+        houses[i+n] = new House(600,yLoc)
+        neighborhood.push(houses[i])
+        neighborhoodLeft.push(houses[i])
+        neighborhood.push(houses[i+n])
+        neighborhoodRight.push(houses[i+n])
+        yLoc += 100
+    }
+}
+
+function makeNewspapers(n){
+    let newspapers = new Array(n)
+    for(let i = 0; i < n; i++){
+        newspapers[i] = new Newspaper()
+        paperArray.push(newspapers[i])
+    }
+}
+
 //checks key press inputs for 4 movement directions
 //need to add a way to keep user within certain boundaries
 function delivererMover() {
-    if(newPlayer.x < 600){
+    if (newPlayer.x < 600) {
         if (pressedKeys.d) {
             newPlayer.x += 2
         }
@@ -194,12 +190,12 @@ function delivererMover() {
             newPlayer.x -= 2
         }
     }
-    if(newPlayer.y > 50){
+    if (newPlayer.y > 50) {
         if (pressedKeys.w) {
             newPlayer.y -= 2
         }
     }
-    if(newPlayer.y < 350){
+    if (newPlayer.y < 350) {
         if (pressedKeys.s) {
             newPlayer.y += 2
         }
@@ -304,7 +300,10 @@ function gameOverCheck() {
 }
 
 function updatePaperCountDisplay() {
-    const sentence = paperArray.length > 0 ? `${paperArray.length} papers left` : "no papers left!"
+    const sentence =
+        paperArray.length > 0
+            ? `${paperArray.length} papers left`
+            : "no papers left!"
     paperCountDisplay.innerText = sentence
 }
 
@@ -312,69 +311,36 @@ function updatePaperCountDisplay() {
 function resetGameState() {
     userScore = 0
 
-    powerup1 = new Powerup(getRandomInBoundsXValue(),getRandomInBoundsYValue())
-    powerup2 = new Powerup(200,200)
-    powerup3 = new Powerup(250,200)
-    powerupArray = [powerup1,powerup2,powerup3]
+    powerup1 = new Powerup(getRandomInBoundsXValue(), getRandomInBoundsYValue())
+    powerup2 = new Powerup(200, 200)
+    powerup3 = new Powerup(250, 200)
+    powerupArray = [powerup1, powerup2, powerup3]
     placedPowerups = []
-
-    leftHouse = new House(5, 5)
-    leftHouse2 = new House(5, 115)
-    leftHouse3 = new House(5, 225)
-    leftHouse4 = new House(5, 335)
-    leftHouse5 = new House(5, 445)
-
-    rightHouse = new House(600, 5)
-    rightHouse2 = new House(600, 100)
-    rightHouse3 = new House(600, 230)
-    rightHouse4 = new House(600, 350)
-    rightHouse5 = new House(600, 460)
 
     newPlayer = new Deliverer(300, 300)
 
-    newPaper = new Newspaper(newPlayer.x, newPlayer.y)
-    newPaper2 = new Newspaper(newPlayer.x, newPlayer.y)
-    newPaper3 = new Newspaper(newPlayer.x, newPlayer.y)
-    newPaper4 = new Newspaper(newPlayer.x, newPlayer.y)
-    newPaper5 = new Newspaper(newPlayer.x, newPlayer.y)
-    paperArray = [newPaper, newPaper2, newPaper3, newPaper4, newPaper5]
-    neighborhood = [
-        leftHouse,
-        rightHouse,
-        leftHouse2,
-        rightHouse2,
-        leftHouse3,
-        rightHouse3,
-        leftHouse4,
-        rightHouse4,
-        leftHouse5,
-        rightHouse5,
-    ]
-    neighborhoodLeft = [
-        leftHouse,
-        leftHouse2,
-        leftHouse3,
-        leftHouse4,
-        leftHouse5,
-    ]
-    neighborhoodRight = [
-        rightHouse,
-        rightHouse2,
-        rightHouse3,
-        rightHouse4,
-        rightHouse5,
-    ]
+    paperArray = []
+    neighborhood = []
+    neighborhoodLeft = []
+    neighborhoodRight = []
     thrownPapersRight = []
     thrownPapersLeft = []
 }
 
 function collectNewspaperCheck() {
-    placedPowerups.forEach((power)=>{
-        if(newPlayer.x < power.x + power.height && /* check player left vs powerup right*/
-            newPlayer.x + newPlayer.width > power.x  && /* check player right vs powerup left */
-            newPlayer.y < power.y + power.height && /* check player top vs powerup bottom */
-            newPlayer.y + newPlayer.height > power.y /* check player bottom vs powerup top */
-            ){ 
+    placedPowerups.forEach((power) => {
+        if (
+            newPlayer.x <
+                power.x +
+                    power.height /* check player left vs powerup right*/ &&
+            newPlayer.x + newPlayer.width >
+                power.x /* check player right vs powerup left */ &&
+            newPlayer.y <
+                power.y +
+                    power.height /* check player top vs powerup bottom */ &&
+            newPlayer.y + newPlayer.height >
+                power.y /* check player bottom vs powerup top */
+        ) {
             console.log("hit bonus newspaper")
             paperArray.push(bonusPaper)
             //temporary code to make powerup go away
@@ -384,28 +350,27 @@ function collectNewspaperCheck() {
     })
 }
 
-function getRandomInBoundsXValue(){
-    return Math.floor(Math.random() * (600 - 100 + 1) +100)
+function getRandomInBoundsXValue() {
+    return Math.floor(Math.random() * (600 - 100 + 1) + 100)
 }
-function getRandomInBoundsYValue(){
-    return Math.floor(Math.random() * (350 - 50 +1)+50)
+function getRandomInBoundsYValue() {
+    return Math.floor(Math.random() * (350 - 50 + 1) + 50)
 }
 
 //places newspapers randomly on level.  would love to have them show up after "x" happens rather than all populate at once.
-function placeExtraNewspapers(){
-    if(powerupArray!= 0){
+function placeExtraNewspapers() {
+    if (powerupArray.length != 0) {
         powerupArray[0].x = getRandomInBoundsXValue()
         powerupArray[0].y = getRandomInBoundsYValue()
         placedPowerups.push(powerupArray[0])
         powerupArray.shift()
     }
-    for(let i = 0; i<placedPowerups.length; i++){
+    for (let i = 0; i < placedPowerups.length; i++) {
         placedPowerups[i].render()
     }
 }
 
 function gameLoop() {
-    //generate houses(neighborhood)
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     scoreBoard.innerText = userScore
     updatePaperCountDisplay()
